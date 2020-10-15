280b98ff8bf84b4c256e767159562bc2de458e10
Torbjørn Ludvigsen <tobben@fastmail.fm>
2020-10-15 19:49:38 +0200

    Updates calibration stills command

--- camera-calibration/pics/takeCalibrationStills.sh
+++ camera-calibration/pics/takeCalibrationStills.sh
@@ -1,1 +1,3 @@
-raspistill --quality 100 --keypress -o camera_calib_%04d.jpg --width 3280 --height 2464
+# You can use the shutter and ISO options to adapt for quite a dark room
+# raspistill -v --quality 100 --keypress --encoding png -o camera_calib_%04d.png --width 3280 --height 2464 --shutter 400000 --ISO 50
+raspistill -v --quality 100 --keypress --encoding png -o camera_calib_%04d.png --width 3280 --height 2464
