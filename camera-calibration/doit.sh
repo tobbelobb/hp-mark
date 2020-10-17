e84d7f8959c7ff65b42569309ef452e3f106a84b
Torbjørn Ludvigsen <tobben@fastmail.fm>
2020-10-17 18:17:37 +0200

    Changes camera calibration example

--- camera-calibration/doit.sh
+++ camera-calibration/doit.sh
@@ -4,9 +4,10 @@
                             --squares_y=11 \
                             --square_side_length=18.145 \
                             --marker_side_length=14.1 \
-                            --images_list=./pics_list.xml \
+                            --images_list=./goodpics_list.xml \
                             --refind_strategy \
                             --detector_params=detector_params.yml \
                             --focal_length=2714.286 \
                             --grid_width=253.8 \
+                            --verbose \
                             myCamParams.xml
