8b76461f1905a08082b5c790c3b2a5aac0e0d973
Torbjørn Ludvigsen <tobben@fastmail.fm>
2020-11-05 12:54:24 +0100

    Collect some totals in benchmark script

--- benchit.sh
+++ benchit.sh
@@ -4,16 +4,35 @@
 
 echo "known_depth estimated_depth difference relative_difference "
 #for known_depth in 1687; do
+total_difference=0
+total_relative_difference=0
+iterations=0
 
 for known_depth in 233 319 482 639 1001 1416 1687; do
-  hpm/hpm/hpm hpm/hpm/example-cam-params/myExampleCamParams.xml 25.84 hpm/hpm/test-images/ball_25_84_dist_${known_depth}_08_Z.png 2>&1 > output
-  n_lines=`wc -l output | awk '{ print $1 }'`
-  if [ 1 -ne $n_lines ];then
-    echo "Error: found ${n_lines} lines of output. Expected 1. See the temporary file 'output' for details"
-    exit
-  fi
-  estimated_depth=`sed -E 's/.+Camera: \([0-9][0-9]+?\.?[0-9]+?, [0-9][0-9]+?\.?[0-9]+?, (.+)\)mm/\1/g' output`
-  difference=`bc -l <<< "${known_depth} - ${estimated_depth}"`
-  relative_difference=`bc -l <<< "1-${known_depth}/${estimated_depth}"`
-  echo "${known_depth} ${estimated_depth} ${difference} ${relative_difference}"
+	hpm/hpm/hpm hpm/hpm/example-cam-params/myExampleCamParams.xml 25.84 hpm/hpm/test-images/ball_25_84_dist_${known_depth}_08_Z.png 2>&1 >output
+	n_lines=$(wc -l output | awk '{ print $1 }')
+	if [ 1 -ne $n_lines ]; then
+		echo "Error: found ${n_lines} lines of output. Expected 1. See the temporary file 'output' for details"
+		exit
+	fi
+	estimated_depth=$(sed -E 's/.+Camera: \([0-9][0-9]+?\.?[0-9]+?, [0-9][0-9]+?\.?[0-9]+?, (.+)\)mm/\1/g' output)
+	difference=$(bc -l <<<"${known_depth} - ${estimated_depth}")
+	relative_difference=$(bc -l <<<"1-${known_depth}/${estimated_depth}")
+	echo "${known_depth} ${estimated_depth} ${difference} ${relative_difference}"
+	total_difference=$(bc -l <<<"define abs(i) {
+                      if (i < 0) return (-i)
+                      return (i)
+                    }
+                    ${total_difference} + abs(${difference})")
+	total_relative_difference=$(bc -l <<<"define abs(i) {
+                               if (i < 0) return (-i)
+                               return (i)
+                             }
+                             ${total_relative_difference} + abs(${relative_difference})")
+	iterations=$(bc -l <<<"${iterations}+1")
 done
+mean_difference=$(bc -l <<<"${total_difference} / ${iterations}")
+mean_relative_difference=$(bc -l <<<"${total_relative_difference} / ${iterations}")
+echo ""
+echo "total_difference total_relative_difference mean_difference mean_relative_difference"
+echo "${total_difference} ${total_relative_difference} ${mean_difference} ${mean_relative_difference}"
