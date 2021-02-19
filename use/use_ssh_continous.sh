b0622ace33d2b1e09c2a3c9f5e35426eb7a34de4
Torbjørn Ludvigsen <tobben@fastmail.fm>
2021-02-19 18:20:47 +0100

    Waits for ssh instead of killing it upon C-c
    Also opts in to lots of VERBOSE output.
    Default use case is that we want to create an Octave matrix,
    or a numpy array or something from the output, so we only
    want hpm's output there usually.

--- use/use_ssh_continous.sh
+++ use/use_ssh_continous.sh
@@ -11,6 +11,9 @@
 # Invokes raspistill and creates random filename for the image.
 # Reads the example params.
 # Forwards -v, -s, -c or any other trailing arguments to hpm.
+#
+# If you want this script to be verbose, say
+# VERBOSE=true ./use_ssh_continous.sh
 
 set -o pipefail
 
@@ -18,10 +21,12 @@ SSH_PID=0
 readonly SSH_PIPE="/tmp/ssh-input-for-pi"
 
 cleanup() {
-	echo "Ran cleanup"
+	if [ ${VERBOSE} ]; then
+		echo "Running cleanup"
+	fi
 	if [ ${SSH_PID} -ne 0 ]; then
-		echo "Killed ssh"
-		kill -s SIGTERM ${SSH_PID}
+		echo "Waiting for ssh"
+		wait ${SSH_PID}
 		rm -f ${SSH_PIPE}
 	fi
 	exit 0
@@ -48,21 +53,33 @@ while true; do
 	mkfifo ${SSH_PIPE}
 	tail -f ${SSH_PIPE} | ssh pi@rpi USEPATH_ON_PI=${USEPATH_ON_PI} IMAGE_ON_PI=${IMAGE_ON_PI} 'bash -s' &
 	SSH_PID=$!
-	echo "cd \"${USEPATH_ON_PI}\" && pwd && \
-        mkdir -p \"${USEPATH_ON_PI}/images\" && \
-        raspistill --quality 100 -o \"${IMAGE_ON_PI}\" --width 3280 --height 2464 && \
-        echo Captured image remotely: \"${IMAGE_ON_PI}\"" >>${SSH_PIPE}
-	echo "exit" >>${SSH_PIPE}
+	PI_CMD="cd \"${USEPATH_ON_PI}\""
+	if [ ${VERBOSE} ]; then
+		PI_CMD+=" && pwd"
+	fi
+	PI_CMD+=" && mkdir -p \"${USEPATH_ON_PI}/images\""
+	PI_CMD+=" && raspistill --quality 100 -o \"${IMAGE_ON_PI}\" --width 3280 --height 2464"
+	if [ ${VERBOSE} ]; then
+		PI_CMD+=" && echo Captured image remotely: \"${IMAGE_ON_PI}\""
+	fi
+	PI_CMD+="; exit"
+	echo ${PI_CMD} >>${SSH_PIPE}
 	wait ${SSH_PID}
 	SSH_PID=0
 
 	cd "${IMAGES}/"
-	echo "Copies home:"
-	scp pi@rpi:${IMAGE_ON_PI} .
-	cd -
+	if [ ${VERBOSE} ]; then
+		echo "Copies home:"
+		scp pi@rpi:${IMAGE_ON_PI} .
+	else
+		scp -q pi@rpi:${IMAGE_ON_PI} .
+	fi
+	cd - >/dev/null
 
 	COMMAND="${HPM} ${CAMPARAMS} ${MARKERPARAMS} ${IMAGE} $@"
-	echo "${COMMAND}"
+	if [ ${VERBOSE} ]; then
+		echo "${COMMAND}"
+	fi
 	$COMMAND
 
 done
