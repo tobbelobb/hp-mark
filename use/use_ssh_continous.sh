3bbb0b308f7f7a3f8c9382de7e879a032329c132
Torbjørn Ludvigsen <tobben@fastmail.fm>
2021-02-25 12:30:43 +0100

    Gives data series better names and collects log

--- use/use_ssh_continous.sh
+++ use/use_ssh_continous.sh
@@ -14,6 +14,12 @@
 #
 # If you want this script to be verbose, say
 # VERBOSE=true ./use_ssh_continous.sh
+#
+# Your will get images saved into a subdirectory ./images/<something>
+# You will also get a log file called ./<something>.log
+# By default, <something> will be set to a random six character name.
+# If you want to set it explicitly do:
+# DATA_SERIES_NAME="my-awesome-data-collection" ./use_ssh_continous.ssh
 
 set -o pipefail
 
@@ -22,10 +28,10 @@ readonly SSH_PIPE="/tmp/ssh-input-for-pi"
 
 cleanup() {
 	if [ ${VERBOSE} ]; then
-		echo "Running cleanup"
+		echo "Running cleanup" 2>&1 | tee /dev/fd/3
 	fi
 	if [ ${SSH_PID} -ne 0 ]; then
-		echo "Waiting for ssh"
+		echo "Waiting for ssh" 2>&1 | tee /dev/fd/3
 		wait ${SSH_PID}
 		rm -f ${SSH_PIPE}
 	fi
@@ -36,7 +42,6 @@ trap cleanup SIGINT SIGTERM
 
 readonly THISPATH="$(dirname "$0")"
 readonly IMAGES="${THISPATH}/images"
-mkdir -p "${IMAGES}/"
 
 readonly USEPATH_ON_PI="/home/pi/repos/hp-mark/use"
 
@@ -44,20 +49,35 @@ readonly HPM="../hpm/hpm/hpm"
 readonly CAMPARAMS="../hpm/hpm/example-cam-params/myExampleCamParams.xml"
 readonly MARKERPARAMS="../hpm/hpm/example-marker-params/my-marker-params.xml"
 
+SERIESNAME=$(mktemp --dry-run XXXXX)
+if [ ${DATA_SERIES_NAME} ]; then
+	SERIESNAME="${DATA_SERIES_NAME}"
+fi
+readonly LOGFILE="${SERIESNAME}.log"
+touch ${LOGFILE}
+exec 3>&1 1>>${LOGFILE} 2>&1
+
+readonly IMAGESERIES="${IMAGES}/${SERIESNAME}"
+mkdir -p "${IMAGESERIES}/"
+
+readonly IMAGESERIES_ON_PI="${USEPATH_ON_PI}/images/${SERIESNAME}"
+
+let "INC=1"
+COUNT=""
 while true; do
-	IMAGENAME=$(mktemp --dry-run XXXXX.jpg)
-	IMAGE="${IMAGES}/${IMAGENAME}"
-	IMAGE_ON_PI="${USEPATH_ON_PI}/images/${IMAGENAME}"
+	printf -v COUNT "%04d" ${INC}
+	IMAGE="${IMAGESERIES}/${COUNT}.jpg"
+	IMAGE_ON_PI="${IMAGESERIES_ON_PI}/${COUNT}.jpg"
 
 	rm -f ${SSH_PIPE}
 	mkfifo ${SSH_PIPE}
-	tail -f ${SSH_PIPE} | ssh pi@rpi USEPATH_ON_PI=${USEPATH_ON_PI} IMAGE_ON_PI=${IMAGE_ON_PI} 'bash -s' &
+	tail -f ${SSH_PIPE} | ssh pi@rpi USEPATH_ON_PI=${USEPATH_ON_PI} IMAGE_ON_PI=${IMAGE_ON_PI} 'bash -s' 2>&1 | tee /dev/fd/3 &
 	SSH_PID=$!
 	PI_CMD="cd \"${USEPATH_ON_PI}\""
 	if [ ${VERBOSE} ]; then
 		PI_CMD+=" && pwd"
 	fi
-	PI_CMD+=" && mkdir -p \"${USEPATH_ON_PI}/images\""
+	PI_CMD+=" && mkdir -p \"${IMAGESERIES_ON_PI}/\""
 	PI_CMD+=" && raspistill --quality 100 -o \"${IMAGE_ON_PI}\" --width 3280 --height 2464"
 	if [ ${VERBOSE} ]; then
 		PI_CMD+=" && echo Captured image remotely: \"${IMAGE_ON_PI}\""
@@ -67,19 +87,18 @@ while true; do
 	wait ${SSH_PID}
 	SSH_PID=0
 
-	cd "${IMAGES}/"
 	if [ ${VERBOSE} ]; then
-		echo "Copies home:"
-		scp pi@rpi:${IMAGE_ON_PI} .
+		echo "Copies home:" 2>&1 | tee /dev/fd/3
+		scp pi@rpi:${IMAGE_ON_PI} ${IMAGE} 2>&1 | tee /dev/fd/3
 	else
-		scp -q pi@rpi:${IMAGE_ON_PI} .
+		scp -q pi@rpi:${IMAGE_ON_PI} ${IMAGE} 2>&1 | tee /dev/fd/3
 	fi
-	cd - >/dev/null
 
 	COMMAND="${HPM} ${CAMPARAMS} ${MARKERPARAMS} ${IMAGE} $@"
 	if [ ${VERBOSE} ]; then
-		echo "${COMMAND}"
+		echo "${COMMAND}" 2>&1 | tee /dev/fd/3
 	fi
-	$COMMAND
+	$COMMAND 2>&1 | tee /dev/fd/3
 
+	let "INC=INC+1"
 done
