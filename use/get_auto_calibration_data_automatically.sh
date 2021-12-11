55c283ff5ca34180a6ad0f2708430babc9e54ca5
Torbjørn Ludvigsen <tobben@fastmail.fm>
2021-12-11 16:52:51 +0100

    Waits for user to push enter between each auto calibration measurement

--- use/get_auto_calibration_data_automatically.sh
+++ use/get_auto_calibration_data_automatically.sh
@@ -77,61 +77,14 @@ readonly READ_ENCODERS="M569.3 P40.0:41.0:42.0:43.0"
 # We assume nozzle is at the origin. Set motor encoder reference point.
 curl --silent ${GCODE_ENDPOINT} -d "${SET_ENCODER_REFERENCE_POINT}" -H "Content-Type: text/plain" >/dev/null
 
-SET_TORQUES=("M98 P\"/macros/Torque_mode\" A0     B0     C0     D0"
-	"M98 P\"/macros/Torque_mode\" A0.025 B0.025 C0.015 D0"
-	"M98 P\"/macros/Torque_mode\" A0.025 B0.025 C0.015 D0"
-	"M98 P\"/macros/Torque_mode\" A0.025 B0.025 C0.015 D0"
-	"M98 P\"/macros/Torque_mode\" A0.025 B0.025 C0.015 D0"
-	"M98 P\"/macros/Torque_mode\" A0.055 B0.055 C0.045 D0"
-	"M98 P\"/macros/Torque_mode\" A0     B0.025 C0.015 D0"
-	"M98 P\"/macros/Torque_mode\" A0     B0.025 C0.015 D0"
-	"M98 P\"/macros/Torque_mode\" A0     B0.055 C0.045 D0"
-	"M98 P\"/macros/Torque_mode\" A0.025 B0     C0.015 D0"
-	"M98 P\"/macros/Torque_mode\" A0.025 B0     C0.015 D0"
-	"M98 P\"/macros/Torque_mode\" A0.055 B0     C0.045 D0"
-	"M98 P\"/macros/Torque_mode\" A0.025 B0.025 C0     D0"
-	"M98 P\"/macros/Torque_mode\" A0.025 B0.025 C0     D0"
-	"M98 P\"/macros/Torque_mode\" A0.055 B0.055 C0     D0"
-)
-
-MOTOR_MOVES=("M98 P\"/macros/Individual_motor_control\" D0    F6000"
-	"M98 P\"/macros/Individual_motor_control\" D-200 F6000"
-	"M98 P\"/macros/Individual_motor_control\" D-200 F6000"
-	"M98 P\"/macros/Individual_motor_control\" D-100 F6000"
-	"M98 P\"/macros/Individual_motor_control\" D-100 F6000"
-	"M98 P\"/macros/Individual_motor_control\" D500  F6000"
-	"M98 P\"/macros/Individual_motor_control\" A-200 F6000"
-	"M98 P\"/macros/Individual_motor_control\" A-200 F6000"
-	"M98 P\"/macros/Individual_motor_control\" A400  F6000"
-	"M98 P\"/macros/Individual_motor_control\" B-200 F6000"
-	"M98 P\"/macros/Individual_motor_control\" B-200 F6000"
-	"M98 P\"/macros/Individual_motor_control\" B400  F6000"
-	"M98 P\"/macros/Individual_motor_control\" C-200 F6000"
-	"M98 P\"/macros/Individual_motor_control\" C-200 F6000"
-	"M98 P\"/macros/Individual_motor_control\" C400  F6000"
-)
-
 for i in "${!SET_TORQUES[@]}"; do
 
 	printf -v COUNT "%04d" ${INC}
 
-	# Set torque
-	curl --silent ${GCODE_ENDPOINT} -d "${SET_TORQUES[i]}" -H "Content-Type: text/plain" >/dev/null
-	# Do motor move
-	curl --silent ${GCODE_ENDPOINT} -d "${MOTOR_MOVES[i]}" -H "Content-Type: text/plain" >/dev/null
-	# Make motors stand still
-	curl --silent ${GCODE_ENDPOINT} -d "M98 P\"/macros/Torque_mode\" A0 B0 C0 D0" -H "Content-Type: text/plain" >/dev/null
+	# Wait for user to push mover around
+	read -p "Press enter to continue"
 
 	MOTOR_POS_SAMP="$(curl --silent ${GCODE_ENDPOINT} -d "${READ_ENCODERS}" -H "Content-Type: text/plain" 2>&1 | tr -d '\n')"
-
-	### Read encoders ###
-	#MOTOR_POS_SAMP="0"
-	#MOTOR_POS_SAMP2="1"
-	#while [ "${MOTOR_POS_SAMP}" != "${MOTOR_POS_SAMP2}" ]; do
-	#	MOTOR_POS_SAMP="$(curl --silent ${GCODE_ENDPOINT} -d "${READ_ENCODERS}" -H "Content-Type: text/plain" 2>&1 | tr -d '\n')"
-	#	sleep 0.5 # Let motors move
-	#	MOTOR_POS_SAMP2="$(curl --silent ${GCODE_ENDPOINT} -d "${READ_ENCODERS}" -H "Content-Type: text/plain" 2>&1 | tr -d '\n')"
-	#done
 	echo -n ${MOTOR_POS_SAMP} | tee /dev/fd/3
 
 	IMAGE="${IMAGESERIES}/${COUNT}.jpg"
